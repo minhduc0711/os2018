5fce989e546b37e9d559f45a325aba2579ec153e
Minh Duc <phamminhduc0711@gmail.com>
2018-01-29 19:38:19 +0700

    Fixed thread joins

--- 08.practical.work.pthread.c
+++ 08.practical.work.pthread.c
@@ -62,9 +62,10 @@ void *consumer(void *para){
 
 
 int main(){
-	pthread_t tid;
-	pthread_create(&tid, NULL, producer, NULL);
-	pthread_create(&tid, NULL, consumer, NULL);
-	pthread_join(tid, NULL);
+	pthread_t tid1,tid2;
+	pthread_create(&tid1, NULL, producer, NULL);
+	pthread_create(&tid2, NULL, consumer, NULL);
+	pthread_join(tid1, NULL);
+	pthread_join(tid2, NULL);
 	return 0;
 }
